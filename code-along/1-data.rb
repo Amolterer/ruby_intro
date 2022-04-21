7d6a6f0105444c362090d3ec573c5ae05005d113
Amolterer <alexander.molterer@kellogg.northwestern.edu>
2022-04-21 00:52:21 +0000

    version1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,31 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
+puts 5 
+puts 3
 # Perform simple math with numbers
-
+#puts 5+3
+#puts 5-3
+#puts 5*3
+#puts 6/2
+#puts 5.0/3.0
 # Strings
-
+puts "Hello, hello!"
+puts "Tacos"
 # Combine strings together
 
 # Variables
+a = 5
+b = 3
+puts a 
+puts a+b
+food = "Tacos"
+quantity =3
+puts food +":" + quantity.to_s
 
 # Combine strings and variables
+puts "#{food}:#{quantity}"
 
 # String manipulation
+puts "Hello" .reverse
+puts "Hello" .upcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,48 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+#this_is_true = true 
+#this_is_false = false
+#puts this_is_true
+#puts this_is_false
 
 # Boolean Expressions
-
+puts 3 == 2 
+puts 3>2
 # If Conditional Logic
+if 3 > 2
+    puts "math works"
+end
+
+if 3 ==2
+    puts "I hope not"
+end
 
 # If/Else Conditional Logic
+your_team_score = 105
+other_team_score = 199
+
+if your_team_score > other_team_score
+    puts "Yay, you won"
+
+elsif your_team_score == other_team_score
+    puts "It's a tie" 
+
+else 
+    puts "boooo!"
+end
 
 # Elsif Conditional Logic
+
+username = "Ben"
+real_username = "Ben"
+password = "Tacos"
+real_password = "puppies"
+ if username == real_username && password == real_password
+    puts "Logged in"
+elsif username == real_username && password!=real_password
+    puts "wrong password"
+end
+
 
 # Combining Expressions
